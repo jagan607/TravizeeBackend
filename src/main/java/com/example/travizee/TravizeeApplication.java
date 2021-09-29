8a6b837283f1dd6efec011e37d5a27fef4b5c45c
jagan607 <jaganmohan19972016@gmail.com>
2021-09-29 21:01:21 +0530

    Project setup

---
+++ src/main/java/com/example/travizee/TravizeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.travizee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravizeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravizeeApplication.class, args);
+	}
+
+}
